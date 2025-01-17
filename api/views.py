a9a0442356f74e728dbda9b2bd81f7ef057de4ad
ghanteyyy <ghanteyyy@gmail.com>
2025-01-17 21:07:52 +0545

    Added view for register endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
 
-# Create your views here.
+
+class Register(APIView):
+    def post(self, request):
+        serialized = UserSerializer(data=request.data)
+        serialized.is_valid(raise_exception=True)
+        user = serialized.save()  # Save the user and related models
+
+        # Serialize the created user instance
+        response_serializer = UserSerializer(user)
+        return Response(response_serializer.data)
