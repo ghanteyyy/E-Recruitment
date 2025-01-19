a68bf333ed40ec22b3f70ccea383e78e2275cd47
ghanteyyy <ghanteyyy@gmail.com>
2025-01-19 19:00:07 +0545

    Improved imports Added new serializers for user_profile_image and user_documents

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,22 @@
 from rest_framework import serializers
-from .models import CustomUser, User_Name, User_Address, User_Contact
+from . import models as models
 
 
 class User_Name_Serializer(serializers.ModelSerializer):
     class Meta:
-        model = User_Name
+        model = models.User_Name
         fields = ['first_name', 'middle_name', 'last_name']
 
 
 class User_Address_Serializer(serializers.ModelSerializer):
     class Meta:
-        model = User_Address
+        model = models.User_Address
         fields = ['address_line1', 'address_line2', 'city', 'state', 'country']
 
 
 class User_Contact_Serializer(serializers.ModelSerializer):
     class Meta:
-        model = User_Contact
+        model = models.User_Contact
         fields = ['phone_number', 'status']
 
 
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
     contact = User_Contact_Serializer(source='user_contact', many=True, read_only=True)
 
     class Meta:
-        model = CustomUser
+        model = models.CustomUser
         fields = ['email', 'password', 'gender', 'dob', 'name', 'address', 'contact']
         extra_kwargs = {
             'password': {'write_only': True},  # Hide password in response
@@ -38,17 +38,57 @@ class UserSerializer(serializers.ModelSerializer):
         address_data = self.initial_data.get('address', [])
         contact_data = self.initial_data.get('contact', [])
 
-        print(name_data, '\n\n', address_data, '\n\n', contact_data)
-
         # Create the main user instance
         password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
+        user = models.CustomUser(**validated_data)
         user.set_password(password)  # Use set_password for hashing
         user.save()
 
         # Create related models (for each item in the lists)
-        User_Name.objects.create(user_id=user, **name_data)
-        User_Address.objects.create(user_id=user, **address_data)
-        User_Contact.objects.create(user_id=user, **contact_data)
+        models.User_Name.objects.create(user_id=user, **name_data)
+        models.User_Address.objects.create(user_id=user, **address_data)
+        models.User_Contact.objects.create(user_id=user, **contact_data)
 
         return user
+
+
+class User_Profiles_Image_Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = models.User_Profile_Images
+        fields = ['image']
+
+    def create(self, validated_data):
+        user = self.context['user']
+
+        try:
+            current_profile_images = models.User_Profile_Images.objects.filter(user_id=user, status='CPP')
+
+            for current_profile_image in current_profile_images:
+                current_profile_image.status = "PPP"
+                current_profile_image.save()
+
+        except models.User_Profile_Images.DoesNotExist:
+            pass
+
+        new_profile_image = models.User_Profile_Images.objects.create(
+                                user_id=user,
+                                image=validated_data.get('image')
+                            )
+
+        return new_profile_image
+
+
+class User_Documents_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User_Documents
+        fields = ['title', 'file']
+
+    def create(self, validated_data):
+        user = self.context['user']
+        user_document = models.User_Documents.objects.create(
+            user_id=user,
+            title=validated_data.get('title'),
+            file=validated_data.get('file')
+        )
+
+        return user_document
