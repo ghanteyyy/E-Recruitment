b8c4ce307b9a9e22c6b29683aa8ce4e26c160eb9
ghanteyyy <ghanteyyy@gmail.com>
2025-02-04 07:55:37 +0545

    Added new url endpoints

--- api/urls.py
+++ api/urls.py
@@ -20,9 +20,10 @@ from . import views
 
 
 urlpatterns = [
+    path('api/register', views.Register.as_view(), name='register'),
     path('api/login', views.Login.as_view(), name='login'),
     path('api/logout', views.Logout.as_view(), name='logout'),
-    path('api/register', views.Register.as_view(), name='register'),
+    path('api/user', views.Users.as_view(), name='user'),
     path('api/profile/image', views.UpdateProfileImage.as_view(), name='profile_image'),
     path('api/user/documents', views.User_Document_view.as_view(), name='documents'),
     path('api/recruiter', views.Recruiter_view.as_view(), name="recruiters"),
